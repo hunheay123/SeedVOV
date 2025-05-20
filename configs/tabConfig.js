055514df0bc304cce10b36e174b8f13fec8cffa3
hunheay <bihunheay1@gmail.com>
2025-05-20 14:01:57 +0900

    Upload react-native_1 project

---
+++ configs/tabConfig.js
@@ -0,0 +1,61 @@
+//import { MaterialCommunityIcons } from "@expo/vector-icons";
+//import HomeScreen from "../screens/HomeScreen";
+//import OCRScreen from "../screens/OCRScreen";
+//import NavigationScreen from "../screens/NavigationScreen";
+
+//const tabConfig = [
+//  {
+//    //name: "Home",
+//    component: HomeScreen,
+//    focusedIcon: "home-variant",
+//    unfocusedIcon: "home-variant-outline",
+//    iconComponent: MaterialCommunityIcons,
+//    renderIcon: (focused) => (
+//      <MaterialCommunityIcons
+//        name={focused ? "home-variant" : "home-variant-outline"}
+//        size={25}
+//        color={focused ? "#ff6347" : "#ddd"}
+//      />
+//    ),
+//    renderText: (focused) => (
+//      <Text style={{ color: focused ? "#ff6347" : "#ddd" }}>홈</Text>
+//    ),
+//  },
+//  {
+//    name: "OCR",
+//    component: OCRScreen,
+//    focusedIcon: "format-text-variant",
+//    unfocusedIcon: "format-text-variant-outline",
+//    iconComponent: MaterialCommunityIcons,
+//    renderIcon: (focused) => (
+//      <MaterialCommunityIcons
+//        name={focused ? "format-text-variant" : "format-text-variant-outline"}
+//        size={25}
+//        color={focused ? "#ff6347" : "#ddd"}
+//      />
+//    ),
+//    renderText: (focused) => (
+//      <Text style={{ color: focused ? "#ff6347" : "#ddd" }}>텍스트 읽기</Text>
+//    ),
+//  },
+//  {
+//    name: "Navigation",
+//    component: NavigationScreen,
+//    focusedIcon: "format-text-variant",
+//    unfocusedIcon: "format-text-variant-outline",
+//    iconComponent: MaterialCommunityIcons,
+//    renderIcon: (focused) => (
+//      <MaterialCommunityIcons
+//        name={focused ? "format-text-variant" : "format-text-variant-outline"}
+//        size={25}
+//        color={focused ? "#ff6347" : "#ddd"}
+//      />
+//    ),
+//    renderText: (focused) => (
+//      <Text style={{ color: focused ? "#ff6347" : "#ddd" }}>텍스트 읽기</Text>
+//    ),
+//  },
+//];
+
+//export default tabConfig;
+// tab이 많으면 동적으로 관리할때 필요
